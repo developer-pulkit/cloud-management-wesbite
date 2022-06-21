ce0f910a86ff03bd3d6b079f23eaae278b4c6980
Pulkit <297pulkitmehta@gmail.com>
2022-06-21 01:36:18 -0400

    Completion of the website

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { CheckIcon } from "@heroicons/react/outline";
+
+function AllInOne() {
+  return (
+    <div name="platforms" className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto px-2">
+        <h2 className="text-5xl font-bold text-center">All-In-One Platform</h2>
+        <p className="text-2xl py-8 text-gray-500 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At corporis
+          eaque mollitia voluptas ab eligendi incidunt nemo totam repellendus,
+          eum provident molestias ipsum iusto, perferendis amet repellat
+          distinctio nulla earum.
+        </p>
+
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            <div className="">
+              <CheckIcon className="w-6 mr-4 text-green-500" />
+            </div>
+            <div className="">
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+                impedit magni magnam perferendis quia dolor consequuntur vel
+                ducimus recusandae minus voluptatum numquam, quasi libero
+                dolore, ullam cumque? Minus, qui impedit?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AllInOne;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
 
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
@@ -12,11 +21,31 @@ function Navbar() {
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
           <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Platforms</li>
-            <li>Pricing</li>
+            <li>
+              <Link to="home" smooth={true} offset={50} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" smooth={true} offset={50} duration={500}>
+                About
+              </Link>
+            </li>{" "}
+            <li>
+              <Link to="support" smooth={true} offset={50} duration={500}>
+                Support
+              </Link>
+            </li>{" "}
+            <li>
+              <Link to="platforms" smooth={true} offset={50} duration={500}>
+                Platforms
+              </Link>
+            </li>{" "}
+            <li>
+              <Link to="pricing" smooth={true} offset={50} duration={500}>
+                Pricing
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
@@ -25,7 +54,7 @@ function Navbar() {
           </button>
           <button className="px-8 py-3">Sign Up</button>
         </div>
-        <div className="md:hidden" onClick={handleClick}>
+        <div className="md:hidden mr-4" onClick={handleClick}>
           {!nav ? (
             <MenuIcon className="mt-2 w-7 cursor-pointer" />
           ) : (
@@ -35,11 +64,66 @@ function Navbar() {
       </div>
 
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-10"}>
-        <li className="border-b-2 border-zinc-300 w-full">Home</li>
-        <li className="border-b-2 border-zinc-300 w-full">About</li>
-        <li className="border-b-2 border-zinc-300 w-full">Support</li>
-        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
-        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+        <li>
+          <Link
+            className="border-b-2 border-zinc-300 w-full"
+            to="home"
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={handleClick}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="border-b-2 border-zinc-300 w-full"
+            to="about"
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={handleClick}
+          >
+            About
+          </Link>
+        </li>{" "}
+        <li>
+          <Link
+            className="border-b-2 border-zinc-300 w-full"
+            to="support"
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={handleClick}
+          >
+            Support
+          </Link>
+        </li>{" "}
+        <li>
+          <Link
+            className="border-b-2 border-zinc-300 w-full"
+            to="platforms"
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={handleClick}
+          >
+            Platforms
+          </Link>
+        </li>{" "}
+        <li>
+          <Link
+            className="border-b-2 border-zinc-300 w-full"
+            to="pricing"
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={handleClick}
+          >
+            Pricing
+          </Link>
+        </li>
         <div className="flex flex-col my-4">
           <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
             Sign In
